b3ba0622f0583a7f466979e271d3d76d619ec331
Hoonsbrand <63503972+Hoonsbrand@users.noreply.github.com>
2022-09-24 22:25:09 +0900

    extension Bundle API KEY

--- YoutubePlayer/YoutubePlayer/Controllers/ViewController.swift
+++ YoutubePlayer/YoutubePlayer/Controllers/ViewController.swift
@@ -105,5 +105,3 @@ extension ViewController {
           }
        }
 }
-
-
--- YoutubePlayer/YoutubePlayer/Models/VideoManager.swift
+++ YoutubePlayer/YoutubePlayer/Models/VideoManager.swift
@@ -12,7 +12,7 @@ class VideoManager {
     var videos = [Item]()
     
     func performRequest(completion: @escaping () -> ()) { 
-        let url = URL(string: "https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&chart=mostPopular&maxResults=25&key=\(YouTubeKey.youTubeKey)")!
+        let url = URL(string: "https://www.googleapis.com/youtube/v3/videos?part=statistics,snippet&chart=mostPopular&maxResults=25&key=\(Bundle.main.YOUTUBE_API_KEY)")!
         
         URLSession.shared.dataTask(with: url){ [weak self] (data, response, error) in
             guard let self = self else { return }
@@ -29,5 +29,22 @@ class VideoManager {
                 completion()
             }
         }.resume()
+    }
+}
+
+extension Bundle {
+    
+    // 생성한 .plist 파일 경로 불러오기
+    var YOUTUBE_API_KEY: String {
+        guard let file = self.path(forResource: "YouTubeInfo", ofType: "plist") else { return "" }
+        
+        // .plist를 딕셔너리로 받아오기
+        guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+        
+        // 딕셔너리에서 값 찾기
+        guard let key = resource["YOUTUBE_API_KEY"] as? String else {
+            fatalError("YOUTUBE_API_KEY error")
+        }
+        return key
     }
 }
